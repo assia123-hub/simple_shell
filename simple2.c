1e4e8621e8378eb84e827026124d4eabb7873787
kata0321 <tpgare3@gmail.com>
2023-11-10 10:08:20 -0800

    task2

--- simple2.c
+++ simple2.c
@@ -4,7 +4,7 @@
 /**
  * main - write a command line arguments
  *@b:buffer store and manipulate the command line arguments
- *@s:strcpy, copies a string,
+ *@s:strcpy copies a string
  *
  * return 0
  */
@@ -18,8 +18,8 @@ return 1;
 char buffer1[100];
 char buffer2[100];
 strcpy(buffer1, av[1]);
-strcpy(buffer2, av[2);
-printf("argument1: %s\n", buffer1);
-printf("argument2: %s\n", buffer2);
+strcpy(buffer2, av[2]);
+printf("Argument 1: %s\n", buffer1);
+printf("Argument 2: %s\n", buffer2);
 return 0;
 }
